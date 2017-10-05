e69033942952995f70889335d21fb0b8664defda
falcon <falcon@falcon>
2017-10-04 22:04:15 -0300

    Primera carga al repositorio de git despues de la presentacion del primer sprint 4-10-17

---
+++ contacto.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<?php
+include_once ("includes/head.php");
+?>
+<body>
+
+	<?php
+	include_once ("includes/nav.php");
+	?>
+
+<section class="titulo-contacto">
+	<h1 style="black" class="contacto">- Contacto -</h1>
+</section>
+
+
+<div class="contenedor-contacto">
+ 	<form>
+
+ 		<div class="nombre-contacto">
+	 	<label for="nombre-contacto">Nombre:</label>
+		<input id="input1" type="text" name="nombre-contacto" required>
+		</div>
+
+		<div class="mail-contacto">
+		<label for="mail-contacto">E-Mail:</label>
+		<input id="input2" type="email" name="mail-contacto" placeholder=" example@example.com" required>
+		</div>
+
+		<div class="asunto-contacto">
+	 	<label for="asunto-contacto">Asunto:</label>
+		<input id="input3" type="text" name="asunto-contacto">
+		</div>
+
+		<div class="mensaje-contacto">
+		<label for="mensaje-contacto">Tu mensaje:</label><br>
+		<textarea name="mensaje-contacto"></textarea>
+		</div>
+
+		<div class="enviar-contacto">
+		<input id="input4" type="submit">
+		</div>
+
+	</form>
+
+</div>
+<?php
+include_once ("includes/footer.php")
+ ?>
+</body>
+</html>
