968dbaa29394a94d97a2feeab44f0189458b3799
falconSP <pacantenys@gmail.com>
2017-10-25 17:00:25 -0300

    arregle MERGE

---
+++ register_ok.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<?php
+include_once ("includes/head.php");
+?>
+<body>
+  <?php
+  include_once ("includes/nav.php");
+
+  ?>
+    <div class="r-ok-contenedor">
+		<span><i class="ion-happy-outline"></i></span>
+			<div class="gracias-r">
+				<h1>¡Gracias por registrarte!</h1>	
+			</div>    </div>
+</body>
+</html>
