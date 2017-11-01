b7b9cad2a91336dce8c333e682d936d4b0b3d966
falconSP <pacantenys@gmail.com>
2017-11-01 11:22:28 -0300

    create table prueba 1

--- createTable.php
+++ createTable.php
@@ -12,7 +12,22 @@ if(! $conn ){
 
 echo 'Connected successfully';
 $sql = 'CREATE Database ecommerce_db';
+
+// PRUEBA
+//      'CREATE TABLE employee( '.
+//      'id INT NOT NULL AUTO_INCREMENT, '.
+//      'name VARCHAR(40) NOT NULL, '.
+//      'email VARCHAR(40) NOT NULL, '.
+//      'age INT NOT NULL, '.
+//      'pass VARCHAR(100)'
+//      'primary key ( id ))';
+//       mysql_select_db('ecommerce_db');
+
+
+
+
 $retval = mysql_query( $sql, $conn );
+
 
 if(! $retval ){
   die('Could not create database: ' . mysql_error());
