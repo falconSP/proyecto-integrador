81e62dd40aed8f0518b33cff9af4ee5b1d748b11
falconSP <pacantenys@gmail.com>
2017-11-04 13:03:41 -0300

    arregle MERGE

---
+++ botones.php
@@ -0,0 +1,34 @@
+<?php
+require_once("funciones2.php");
+
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Crear DB</title>
+<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+	<div class="super-contenedor-botones">
+		<img class="imagen-botones" src="images/hombre-compu.jpg" alt="database">
+		<div class="contenedor-botones">
+			<form method="post">
+				<button class="boton-db" name="crearDB" value="create">Crear DB</button>
+				<button class="boton-tabla" name="crearTable" value="">Crear Tabla</button>
+				<button class="boton-migrar" name="migrarDatos" value="">Migrar datos</button>
+				<a class="salir-botones" href="index.php">Volver al index</a>
+			</form>	
+		</div>
+	</div>
+	
+	
+
+
+
+
+
+</body>
+</html>
---
+++ funciones2.php
@@ -0,0 +1,104 @@
+<?php
+//en la linea 3 habra que modificar el link para que ande!!
+require_once('funciones.php');
+
+if(isset($_POST['crearDB'])){
+	$dsn = 'mysql:host=localhost;'; // Sin db name
+	$db_username = 'root';
+	$db_password = ''; //habra que modificar el password dependiendo de la conexion de cada pc
+
+	try {
+		$conn = new PDO($dsn, $db_username, $db_password);
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
+
+	if (isset($conn)) {
+		$sql = "CREATE SCHEMA `EcommerceDB`";
+		$conn->prepare($sql);
+		$conn->exec($sql);
+		echo "DB created successfully";
+	} else {
+		echo "No se creo la DB";
+	}
+
+//creamos la tabla
+}elseif (isset($_POST['crearTable'])) {
+	$dsn = 'mysql:host=localhost;dbname=EcommerceDB;';
+	$db_username = 'root';
+	$db_password = '';
+
+	try {
+		$conn = new PDO($dsn, $db_username, $db_password);
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
+
+	if (isset($conn)) {
+		$sql =
+
+		'CREATE TABLE `EcommerceDB`.`users` (
+		  `id` INT NOT NULL AUTO_INCREMENT,
+		  `name` VARCHAR(40) NOT NULL,
+			`email` VARCHAR(40) NOT NULL,
+			`age` INT NOT NULL,
+			`pass` VARCHAR(100) NOT NULL,
+		  PRIMARY KEY (`id`)); ';
+
+
+		$conn->prepare($sql);
+		$conn->exec($sql);
+		echo "Tabla creada exitosamente!";
+	} else {
+		echo "No se creo la Tabla";
+	}
+}
+
+//  a partir de aca es para Migrar los datos del json a Mysql
+elseif (isset($_POST['migrarDatos'])) {
+	$dsn = 'mysql:host=localhost;dbname=EcommerceDB;';
+	$db_username = 'root';
+	$db_password = '';
+
+	try {
+		$conn = new PDO($dsn, $db_username, $db_password);
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
+
+	if (isset($conn)) {
+//traigo los usuarios del json usando la funcion del 1er archivo de funciones y las dejo en la variable $usuarios
+		$usuarios = traerTodos();
+
+		foreach ($usuarios as $unUsuario) {
+
+// En $sql lo que hice fue unir el string con las variables que me iba dando el foreach con la variable $usuarios para agregar individualmente a cada uno
+			$sql = "USE EcommerceDB; INSERT INTO users(name, email, age, pass)
+			VALUES (:name, :email, :age, :pass)";
+			$stmt= $conn->prepare($sql);
+			$stmt->bindValue(":name", $unUsuario['name']);
+			$stmt->bindValue(":email", $unUsuario['email']);
+			$stmt->bindValue(":age", $unUsuario['age']);
+			$stmt->bindValue(":pass", $unUsuario['pass']);
+			$stmt->execute();
+		}
+
+		echo "Datos migrados exitosamente!";
+	} else {
+		echo "No se lograron migrar los datos";
+	}
+}
+
+
+function hayConexion(){
+$dsn = 'mysql:host=localhost;dbname=EcommerceDB;';
+$db_username = 'root';
+$db_password = '';
+
+try {
+	$conn = new PDO($dsn, $db_username, $db_password);
+} catch(PDOException $e) {
+	echo $e->getMessage();
+}
+return $conn;
+}
